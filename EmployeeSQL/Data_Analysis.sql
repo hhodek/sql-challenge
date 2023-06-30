0e49f3b51298f1d8d40513aae368babe043478b9
hhodek <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>
2023-06-30 01:05:23 -0500

    data analysis pt 2

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -18,9 +18,42 @@ FROM departments AS d
 JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
 JOIN employees AS e ON dm.emp_no = e.emp_no;
 
+
 ---- List the department number for each employee along with that employee’s 
 ---- employee number, last name, first name, and department name.
 SELECT d.dept_no, e.first_name, e.last_name, de.emp_no, d.dept_name
 FROM employees AS e
 JOIN dept_emp AS de ON e.emp_no = de.emp_no
 join departments AS d ON de.dept_no = d.dept_no;
+
+
+---- List first name, last name, and sex of each employee whose first name is
+---- Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+
+---- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+
+
+---- List each employee in the Sales and Development departments, including their employee number, 
+---- last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development')
+
+
+---- List the frequency counts, in descending order, of all the employee last names 
+----(that is, how many employees share each last name).
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC
--- EmployeeSQL/Data_Engineering.sql
+++ EmployeeSQL/Data_Engineering.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees (
   birth_date DATE NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  gender CHAR(1) NOT NULL,
+  sex VARCHAR(3) NOT NULL,
   hire_date DATE NOT NULL,
   FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
