4d78ccdbb5c82a533520a1e10b71993b39594b38
hhodek <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>
2023-07-05 20:35:19 -0500

    Updated Readme andfiles

--- EmployeeSQL/Table_Schemas.sql
+++ EmployeeSQL/Table_Schemas.sql
@@ -1,23 +1,23 @@
 --Create Departments table
 CREATE TABLE departments (
-  dept_no VARCHAR(4) PRIMARY KEY,
+  dept_no CHAR(4) PRIMARY KEY,
   dept_name VARCHAR(50) NOT NULL
 );
 
 --Create Titles table
 CREATE TABLE titles (
-  title_id VARCHAR(5) PRIMARY KEY,
+  title_id VARCHAR(10) PRIMARY KEY,
   title VARCHAR(50) NOT NULL
 );
 
 --Create Employees table
 CREATE TABLE employees (
   emp_no INT PRIMARY KEY,
-  emp_title_id VARCHAR(5),
+  emp_title_id VARCHAR(10) NOT NULL,
   birth_date DATE NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  sex VARCHAR(3) NOT NULL,
+  sex CHAR(1) NOT NULL,
   hire_date DATE NOT NULL,
   FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
@@ -26,6 +26,7 @@ CREATE TABLE employees (
 CREATE TABLE dept_emp (
   emp_no INT,
   dept_no VARCHAR(4),
+  PRIMARY KEY (emp_no, dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
@@ -34,6 +35,7 @@ CREATE TABLE dept_emp (
 CREATE TABLE dept_manager (
   dept_no VARCHAR(4),
   emp_no INT,
+  PRIMARY KEY (dept_no, emp_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
